dba4e443e97825f317b4d023c5512d6768fa7ea9
Gabriel Velasco <gabriel.velasco001@gmail.com>
2016-07-12 06:49:25 -0400

    Did some more tests, needs refactoring

--- lua/spec/prime_factors_spec.lua
+++ lua/spec/prime_factors_spec.lua
@@ -6,7 +6,22 @@ Tests for the prime factors class
 local prime_factors = require 'src/prime_factors'
 
 describe('Prime factors', function()
-  it('one should have no prime factors', function() -- @3:53
-    assert.are.same({}, prime_factors.compute_factors_for(1));
+  it('one should have no prime factors', function()
+    assert.are.same({}, prime_factors.compute_factors_for(1))
   end)
+
+  it('two has only itself as a prime factor', function()
+    assert.are.same({ 2 }, prime_factors.compute_factors_for(2))
+  end)
+
+  it('three has only itself as a prime factor', function()
+    assert.are.same({ 3 }, prime_factors.compute_factors_for(3))
+  end)
+
+  it('four has two times two as prime factors', function()
+    assert.are.same({ 2, 2 }, prime_factors.compute_factors_for(4))
+  end)
+
+  --@7:52
+  it('five', function() assert.is_true(false) end)
 end)
--- lua/src/prime_factors.lua
+++ lua/src/prime_factors.lua
@@ -3,6 +3,18 @@ Results for the prime factors class
 --]]
 
 function compute_factors_for(number)
+  if number > 1 then
+    factors = {}
+    if number % 2 == 0 then
+      table.insert(factors, 2)
+      number = number / 2;
+    end
+    if number > 1 then
+      table.insert(factors,number)
+    end
+    return factors
+  end
+
   return {}
 end
 
