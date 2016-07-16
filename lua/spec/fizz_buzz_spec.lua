378f38792109a491820172ba9f333b2426476346
Gabriel Velasco <gabriel.velasco001@gmail.com>
2016-07-16 08:20:15 -0400

    Finished fizz buzz kata

--- lua/spec/fizz_buzz_spec.lua
+++ lua/spec/fizz_buzz_spec.lua
@@ -5,20 +5,14 @@ Tests for the fizz buzz module
 local fizz_buzz = require 'src/fizz_buzz'
 
 describe('fizz buzz', function()
-  it('should return 1 as a number', function()
+  it('should return number if number is not divisible by 3 or 5', function()
     assert.is.equal(1, fizz_buzz(1))
-  end)
-
-  it('should return 2 as a number', function()
     assert.is.equal(2, fizz_buzz(2))
+    assert.is.equal(4, fizz_buzz(4))
   end)
 
   it('should return fizz if the number is 3', function()
     assert.is.equal('fizz', fizz_buzz(3))
-  end)
-
-  it('should return 4 as a number', function()
-    assert.is.equal(4, fizz_buzz(4))
   end)
 
   it('should return buzz if the number is 5', function()
@@ -27,5 +21,18 @@ describe('fizz buzz', function()
 
   it('should return fizz buzz if the number is divisible by 3 and 5', function()
     assert.is.equal('fizz buzz', fizz_buzz(7 * 5 * 3))
+  end)
+
+  it('should assert for 0 and below', function()
+    local error_input_0 = function()
+      fizz_buzz(0)
+    end
+
+    local error_input_below_0 = function()
+      fizz_buzz(-2)
+    end
+
+    assert.has_error(error_input_0, 'Error: number must be > 0')
+    assert.has_error(error_input_below_0, 'Error: number must be > 0')
   end)
 end)
--- lua/src/fizz_buzz.lua
+++ lua/src/fizz_buzz.lua
@@ -2,16 +2,31 @@
 Simple fizz buzz implementation
 --]]
 
-return function(number)
+local function is_multiple_of(divisor, number)
+  return number % divisor == 0
+end
+
+local function check_that_number_is_greater_than_zero(number)
+  if number <= 0 then
+    error('Error: number must be > 0')
+  end
+end
+
+local function figure_out_result(number)
   local result = number
 
-  if number % 3 == 0 and number % 5 == 0 then
+  if is_multiple_of(3, number) and is_multiple_of(5, number) then
     result = 'fizz buzz'
-  elseif number % 3 == 0 then
+  elseif is_multiple_of(3, number) then
     result = 'fizz'
-  elseif number % 5 == 0 then
+  elseif is_multiple_of(5, number) then
     result = 'buzz'
   end
 
   return result
+end
+
+return function(number)
+  check_that_number_is_greater_than_zero(number)
+  return figure_out_result(number)
 end
