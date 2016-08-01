44ea6467a1197d51456c374b554993820020fadc
Gabriel Velasco <gabriel.velasco001@gmail.com>
2016-08-01 06:38:59 -0400

    Implemented all the rules, first episode done. Still needs some work.

--- lua/spec/conways_game_of_life/Cell_spec.lua
+++ lua/spec/conways_game_of_life/Cell_spec.lua
@@ -3,22 +3,84 @@ local life_state = require 'src/conways_game_of_life/life_state_enum'
 
 describe('cell', function()
   it('should be alive on initialization', function()
-    local cell = Cell();
+    local cell = Cell(life_state.alive);
+
+    assert.are.equal(life_state.alive, cell.life_state)
+  end)
+
+  it('should be dead on initialization', function()
+    local cell = Cell(life_state.dead);
 
     assert.are.equal(life_state.alive, cell.life_state)
   end)
 
   it('should die with only one neighbor', function()
-    local cell = Cell()
+    local cell = Cell(life_state.alive)
 
-    local actual = cell:get_next_state(1)
+    local actual = cell:get_next_state_when_number_of_neighbors_is(1)
     assert.are.equal(life_state.dead, actual)
   end)
 
   it('should die with zero neighbors', function()
-    local cell = Cell()
+    local cell = Cell(life_state.alive)
 
-    local actual = cell:get_next_state(0)
+    local actual = cell:get_next_state_when_number_of_neighbors_is(0)
     assert.are.equal(life_state.dead, actual)
+  end)
+
+  it('should survive if the cell has two neighbors', function()
+    local cell = Cell(life_state.alive)
+
+    local actual = cell:get_next_state_when_number_of_neighbors_is(2)
+    assert.are.equal(life_state.alive, actual)
+  end)
+
+  it('should survive if the cell has three neighbors', function()
+    local cell = Cell(life_state.alive)
+
+    local actual = cell:get_next_state_when_number_of_neighbors_is(3)
+    assert.are.equal(life_state.alive, actual)
+  end)
+
+  it('should die with four neighbors', function()
+    local cell = Cell(life_state.alive)
+
+    local actual = cell:get_next_state_when_number_of_neighbors_is(4)
+    assert.are.equal(life_state.dead, actual)
+  end)
+
+  it('should die with five neighbors', function()
+    local cell = Cell(life_state.alive)
+
+    local actual = cell:get_next_state_when_number_of_neighbors_is(5)
+    assert.are.equal(life_state.dead, actual)
+  end)
+
+  it('should die with six neighbors', function()
+    local cell = Cell(life_state.alive)
+
+    local actual = cell:get_next_state_when_number_of_neighbors_is(6)
+    assert.are.equal(life_state.dead, actual)
+  end)
+
+  it('should die with seven neighbors', function()
+    local cell = Cell(life_state.alive)
+
+    local actual = cell:get_next_state_when_number_of_neighbors_is(7)
+    assert.are.equal(life_state.dead, actual)
+  end)
+
+  it('should die with eight neighbors', function()
+    local cell = Cell(life_state.alive)
+
+    local actual = cell:get_next_state_when_number_of_neighbors_is(8)
+    assert.are.equal(life_state.dead, actual)
+  end)
+
+  it('should become alive with three neighbors', function()
+    local cell = Cell(life_state.dead)
+
+    local actual = cell:get_next_state_when_number_of_neighbors_is(3)
+    assert.are.equal(life_state.alive, actual)
   end)
 end)
--- lua/src/conways_game_of_life/Cell.lua
+++ lua/src/conways_game_of_life/Cell.lua
@@ -1,12 +1,16 @@
 local life_state = require 'src/conways_game_of_life/life_state_enum'
 
-local function get_next_state(cell, number_of_neighbors)
+local function get_next_state_when_number_of_neighbors_is(cell, number_of_neighbors)
+  if number_of_neighbors > 1 and number_of_neighbors < 4 then
+    return life_state.alive
+  end
+
   return life_state.dead
 end
 
-return function()
+return function(state)
     return {
     life_state = life_state.alive,
-    get_next_state = get_next_state
+    get_next_state_when_number_of_neighbors_is = get_next_state_when_number_of_neighbors_is
   }
 end
