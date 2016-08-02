d875aa033da4158186432598a6954bc307146464
Gabriel Velasco <gabriel.velasco001@gmail.com>
2016-08-02 06:22:23 -0400

    Added a couple more tests

--- lua/spec/conways_game_of_life/Cell_spec.lua
+++ lua/spec/conways_game_of_life/Cell_spec.lua
@@ -11,20 +11,20 @@ describe('cell', function()
   it('should be dead on initialization', function()
     local cell = Cell(life_state.dead);
 
-    assert.are.equal(life_state.alive, cell.life_state)
-  end)
-
-  it('should die with only one neighbor', function()
-    local cell = Cell(life_state.alive)
-
-    local actual = cell:get_next_state_when_number_of_neighbors_is(1)
-    assert.are.equal(life_state.dead, actual)
+    assert.are.equal(life_state.dead, cell.life_state)
   end)
 
   it('should die with zero neighbors', function()
     local cell = Cell(life_state.alive)
 
     local actual = cell:get_next_state_when_number_of_neighbors_is(0)
+    assert.are.equal(life_state.dead, actual)
+  end)
+
+  it('should die with only one neighbor', function()
+    local cell = Cell(life_state.alive)
+
+    local actual = cell:get_next_state_when_number_of_neighbors_is(1)
     assert.are.equal(life_state.dead, actual)
   end)
 
@@ -77,10 +77,66 @@ describe('cell', function()
     assert.are.equal(life_state.dead, actual)
   end)
 
+  it('should stay dead with zero neighbors', function()
+    local cell = Cell(life_state.dead)
+
+    local actual = cell:get_next_state_when_number_of_neighbors_is(0)
+    assert.are.equal(life_state.dead, actual)
+  end)
+
+  it('should stay dead with one neighbors', function()
+    local cell = Cell(life_state.dead)
+
+    local actual = cell:get_next_state_when_number_of_neighbors_is(1)
+    assert.are.equal(life_state.dead, actual)
+  end)
+
+  it('should stay dead with two neighbors', function()
+    local cell = Cell(life_state.dead)
+
+    local actual = cell:get_next_state_when_number_of_neighbors_is(2)
+    assert.are.equal(life_state.dead, actual)
+  end)
+
   it('should become alive with three neighbors', function()
     local cell = Cell(life_state.dead)
 
     local actual = cell:get_next_state_when_number_of_neighbors_is(3)
     assert.are.equal(life_state.alive, actual)
+  end)
+
+  it('should stay dead with four neighbors', function()
+    local cell = Cell(life_state.dead)
+
+    local actual = cell:get_next_state_when_number_of_neighbors_is(4)
+    assert.are.equal(life_state.dead, actual)
+  end)
+
+  it('should stay dead with five neighbors', function()
+    local cell = Cell(life_state.dead)
+
+    local actual = cell:get_next_state_when_number_of_neighbors_is(5)
+    assert.are.equal(life_state.dead, actual)
+  end)
+
+  it('should stay dead with six neighbors', function()
+    local cell = Cell(life_state.dead)
+
+    local actual = cell:get_next_state_when_number_of_neighbors_is(6)
+    assert.are.equal(life_state.dead, actual)
+  end)
+
+  it('should stay dead with seven neighbors', function()
+    local cell = Cell(life_state.dead)
+
+    local actual = cell:get_next_state_when_number_of_neighbors_is(7)
+    assert.are.equal(life_state.dead, actual)
+  end)
+
+  it('should stay dead with eight neighbors', function()
+    local cell = Cell(life_state.dead)
+
+    local actual = cell:get_next_state_when_number_of_neighbors_is(8)
+    assert.are.equal(life_state.dead, actual)
   end)
 end)
--- lua/src/conways_game_of_life/Cell.lua
+++ lua/src/conways_game_of_life/Cell.lua
@@ -1,8 +1,14 @@
 local life_state = require 'src/conways_game_of_life/life_state_enum'
 
 local function get_next_state_when_number_of_neighbors_is(cell, number_of_neighbors)
-  if number_of_neighbors > 1 and number_of_neighbors < 4 then
-    return life_state.alive
+  if cell.life_state == life_state.alive then
+    if number_of_neighbors > 1 and number_of_neighbors < 4 then
+      return life_state.alive
+    end
+  else
+    if number_of_neighbors == 3 then
+      return life_state.alive
+    end
   end
 
   return life_state.dead
@@ -10,7 +16,7 @@ end
 
 return function(state)
     return {
-    life_state = life_state.alive,
+    life_state = state,
     get_next_state_when_number_of_neighbors_is = get_next_state_when_number_of_neighbors_is
   }
 end
