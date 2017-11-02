e27c9a30c1a3201271019b1bbc08882c8fce6015
Gabriel Velasco <gabriel.velasco001@gmail.com>
2017-11-02 06:27:16 -0400

    Refactored to hopefully make it better

--- lua/spec/sorting/bubble-sort_spec.lua
+++ lua/spec/sorting/bubble-sort_spec.lua
@@ -51,7 +51,7 @@ procedure bubbleSort( A : list of sortable items )
 end procedure
 ]]
 
-describe('Bubble sort', function()
+describe('bubble sort', function()
   local bubble_sort = require 'src/sorting/bubble-sort'
 
   it('should sort two numbers in ascending order', function()
@@ -63,12 +63,24 @@ describe('Bubble sort', function()
   end)
 
   it('should sort a set of numbers in ascending order', function()
-    assert.are.same({-3, -3, -2.2, 0, 4, 4.9, 4.9, 7, 10}, bubble_sort.ascending({10, 4.9, -3, 4, 0, -2.2, 4.9, 7, -3}))
+    assert.are.same({ -3, -3, -2.2, 0, 4, 4.9, 4.9, 7, 10 }, bubble_sort.ascending({ 10, 4.9, -3, 4, 0, -2.2, 4.9, 7, -3 }))
   end)
 
   it('should not modify the original set', function()
-    local actual = {4, 2, 3, 1}
-    bubble_sort.ascending(actual)
-      assert.are.same({4, 2, 3, 1}, actual)
+    local actual = { 4, 2, 3, 1 }
+    bubble_sort.descending(actual)
+    assert.are.same({ 4, 2, 3, 1 }, actual)
+  end)
+
+  it('should sort two numbers in descending order', function()
+    assert.are.same({ 2, 1 }, bubble_sort.descending({ 2, 1 }))
+  end)
+
+  it('should sort three numbers in descending order', function()
+    assert.are.same({ 2, 1, -1 }, bubble_sort.descending({ 1, -1, 2 }))
+  end)
+
+  it('should sort a set of numbers in descending order', function()
+    assert.are.same({ 10, 7, 4.9, 4.9, 4, 0, -2.2, -3, -3 }, bubble_sort.descending({ 10, 4.9, -3, 4, 0, -2.2, 4.9, 7, -3 }))
   end)
 end)
--- lua/src/sorting/bubble-sort.lua
+++ lua/src/sorting/bubble-sort.lua
@@ -1,20 +1,24 @@
 local function make_copy(set)
-  local _set = {}
-  for i = 1, #set do
-    table.insert(_set, set[i])
+  local copy = {}
+  for k in ipairs(set) do
+    table.insert(copy, set[k])
   end
-  return _set
+  return copy
 end
 
-local function ascending(set)
+local function swap_values(set, position1, position2)
+  local temp = set[position1]
+  set[position1] = set[position2]
+  set[position2] = temp
+end
+
+local function sort(set, order)
   local copy = make_copy(set)
 
   for k in ipairs(set) do
     for w, v in ipairs(copy) do
-      if w > 1 and copy[w - 1] > copy[w] then
-        local temp = copy[w - 1]
-        copy[w - 1] = copy[w]
-        copy[w] = temp
+      if w > 1 and order(copy[w - 1], copy[w]) then
+        swap_values(copy, w - 1, w)
       end
     end
   end
@@ -22,6 +26,15 @@ local function ascending(set)
   return copy
 end
 
+local function ascending(set)
+  return sort(set, function(value1, value2) return value1 > value2 end)
+end
+
+local function descending(set)
+  return sort(set, function(value1, value2) return value1 < value2 end)
+end
+
 return {
-  ascending = ascending
+  ascending = ascending,
+  descending = descending
 }
