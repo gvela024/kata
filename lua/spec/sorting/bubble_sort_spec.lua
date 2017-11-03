88f7bb1531f209010ccac4e1a1fbd9e3054c4536
Gabriel Velasco <gabriel.velasco001@gmail.com>
2017-11-03 08:14:00 -0400

    renamed files and objects to use underscores instead of hyphens for names

--- lua/spec/sorting/bubble_sort_spec.lua
+++ lua/spec/sorting/bubble_sort_spec.lua
@@ -4,55 +4,10 @@ algorithm that repeatedly steps through the list to be sorted, compares
 each pair of adjacent items and swaps them if they are in the wrong order.
 The pass through the list is repeated until no swaps are needed, which
 indicates that the list is sorted.
-
-procedure bubbleSort( A : list of sortable items )
-   n = length(A)
-   repeat
-     swapped = false
-     for i = 1 to n-1 inclusive do
-       /* if this pair is out of order */
-       if A[i-1] > A[i] then
-         /* swap them and remember something changed */
-         swap( A[i-1], A[i] )
-         swapped = true
-       end if
-     end for
-   until not swapped
-end procedure
-
-Optimization #1
-procedure bubbleSort( A : list of sortable items )
-    n = length(A)
-    repeat
-       swapped = false
-       for i = 1 to n-1 inclusive do
-          if A[i-1] > A[i] then
-             swap(A[i-1], A[i])
-             swapped = true
-          end if
-       end for
-       n = n - 1
-    until not swapped
-end procedure
-
-Optimization #2
-procedure bubbleSort( A : list of sortable items )
-    n = length(A)
-    repeat
-       newn = 0
-       for i = 1 to n-1 inclusive do
-          if A[i-1] > A[i] then
-             swap(A[i-1], A[i])
-             newn = i
-          end if
-       end for
-       n = newn
-    until n = 0
-end procedure
 ]]
 
 describe('bubble sort', function()
-  local bubble_sort = require 'src/sorting/bubble-sort'
+  local bubble_sort = require 'src/sorting/bubble_sort'
 
   it('should sort two numbers in ascending order', function()
     assert.are.same({ 1, 2 }, bubble_sort.ascending({ 2, 1 }))
---
+++ lua/spec/sorting/selection_sort_spec.lua
@@ -0,0 +1,53 @@
+--[[
+The algorithm divides the input list into two parts: the sublist of items
+already sorted, which is built up from left to right at the front (left)
+of the list, and the sublist of items remaining to be sorted that occupy
+the rest of the list. Initially, the sorted sublist is empty and the unsorted
+sublist is the entire input list. The algorithm proceeds by finding the
+smallest (or largest, depending on sorting order) element in the unsorted
+sublist, exchanging (swapping) it with the leftmost unsorted element
+(putting it in sorted order), and moving the sublist boundaries one element
+to the right.
+
+Example
+64 25 12 22 11 // this is the initial, starting state of the array
+11 25 12 22 64 // sorted sublist = {11}
+11 12 25 22 64 // sorted sublist = {11, 12}
+11 12 22 25 64 // sorted sublist = {11, 12, 22}
+11 12 22 25 64 // sorted sublist = {11, 12, 22, 25}
+11 12 22 25 64 // sorted sublist = {11, 12, 22, 25, 64}
+]]
+
+describe('bubble sort', function()
+  local selection_sort = require 'src/sorting/bubble_sort'
+
+  it('should sort two numbers in ascending order', function()
+    assert.are.same({ 1, 2 }, bubble_sort.ascending({ 2, 1 }))
+  end)
+
+  it('should sort three numbers in ascending order', function()
+    assert.are.same({ -1, 1, 2 }, bubble_sort.ascending({ 1, -1, 2 }))
+  end)
+
+  it('should sort a set of numbers in ascending order', function()
+    assert.are.same({ -3, -3, -2.2, 0, 4, 4.9, 4.9, 7, 10 }, bubble_sort.ascending({ 10, 4.9, -3, 4, 0, -2.2, 4.9, 7, -3 }))
+  end)
+
+  it('should not modify the original set', function()
+    local actual = { 4, 2, 3, 1 }
+    bubble_sort.descending(actual)
+    assert.are.same({ 4, 2, 3, 1 }, actual)
+  end)
+
+  it('should sort two numbers in descending order', function()
+    assert.are.same({ 2, 1 }, bubble_sort.descending({ 2, 1 }))
+  end)
+
+  it('should sort three numbers in descending order', function()
+    assert.are.same({ 2, 1, -1 }, bubble_sort.descending({ 1, -1, 2 }))
+  end)
+
+  it('should sort a set of numbers in descending order', function()
+    assert.are.same({ 10, 7, 4.9, 4.9, 4, 0, -2.2, -3, -3 }, bubble_sort.descending({ 10, 4.9, -3, 4, 0, -2.2, 4.9, 7, -3 }))
+  end)
+end)
