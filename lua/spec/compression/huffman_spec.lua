bebdf8cc7cad5677c4c573f3bb94d22c094cd100
Gabriel Velasco <gabriel.velasco@geappliances.com>
2020-05-04 07:17:27 -0400

    Ugh, weirdness

--- lua/spec/compression/huffman_spec.lua
+++ lua/spec/compression/huffman_spec.lua
@@ -6,26 +6,39 @@ https://people.ok.ubc.ca/ylucet/DS/Huffman.html
 describe('huffman', function()
   local Huffman = require 'src.compression.huffman'
 
-  it('should create unordered map', function()
+  -- it('should create unordered map', function()
+  --   local huffman = Huffman('aaabbc')
+
+  --   assert.are.same({ a = 3, b = 2, c = 1 }, huffman.unordered_map)
+  -- end)
+
+  -- it('should create a priority queue', function()
+  --   local huffman = Huffman('aaabbc')
+
+  --   -- choosing not to test this guy since the order
+  --   -- in which the unordered tree is parsed isn't guaranteed...
+  --   -- in lua anyway
+  --   -- assert.are.same(
+  --   --   {
+  --   --     { character = 'a', frequency = 3 },
+  --   --     { character = 'b', frequency = 2 },
+  --   --     { character = 'c', frequency = 1 }
+  --   --   },
+  --   --   huffman.priority_queue
+  --   -- )
+  -- end)
+
+  it('should create the huffman codes', function()
     local huffman = Huffman('aaabbc')
 
-    assert.are.same({ a = 3, b = 2, c = 1 }, huffman.unordered_map)
-  end)
-
-  it('should create a priority queue', function()
-    local huffman = Huffman('aaabbc')
-
-    -- choosing not to test this guy since the order
-    -- in which the unordered tree is parsed isn't guaranteed...
-    -- in lua anyway
-    -- assert.are.same(
-    --   {
-    --     { character = 'a', frequency = 3 },
-    --     { character = 'b', frequency = 2 },
-    --     { character = 'c', frequency = 1 }
-    --   },
-    --   huffman.priority_queue
-    -- )
+    assert.are.same(
+      {
+        b = '11',
+        a = '0',
+        c = '10'
+      },
+      huffman.huffman_codes
+    )
   end)
 
   -- it('should encode characters', function()
--- lua/src/compression/huffman.lua
+++ lua/src/compression/huffman.lua
@@ -1,3 +1,5 @@
+local heap_utilities = require 'src.sorting.heap_utilities'
+
 local function build_unordered_map(text, unordered_map)
   for c in text:gmatch"." do
     if unordered_map[c] then
@@ -24,17 +26,39 @@ local function build_priority_queue(unordered_map, priority_queue)
   end
 end
 
+local function build_huffman_codes(priority_queue, node_index, code, huffman_codes)
+  print()
+  print(node_index)
+
+  if node_index > #priority_queue then return end
+
+  local left_child_value, left_child_index = heap_utilities.get_left_child(node_index, priority_queue)
+  local right_child_value, right_child_index = heap_utilities.get_right_child(node_index, priority_queue)
+
+  print(left_child_value, right_child_value)
+  print(left_child_index, right_child_index)
+  -- for k, v in pairs(left_child_value) do print(k, v) end
+
+  if left_child_value == nil and right_child_value == nil then huffman_codes[priority_queue[node_index].character] = code end
+
+  build_huffman_codes(priority_queue, left_child_index, code .. '0', huffman_codes)
+  build_huffman_codes(priority_queue, right_child_index, code .. '1', huffman_codes)
+end
+
 return function(text)
   local codes = {}
   local unordered_map = {}
   local priority_queue = {}
+  local huffman_codes = {}
 
   build_unordered_map(text, unordered_map)
   build_priority_queue(unordered_map, priority_queue)
+  build_huffman_codes(priority_queue, 1, '', huffman_codes)
 
   return {
     codes = codes,
     unordered_map = unordered_map,
-    priority_queue = priority_queue
+    priority_queue = priority_queue,
+    huffman_codes = huffman_codes
   }
 end
